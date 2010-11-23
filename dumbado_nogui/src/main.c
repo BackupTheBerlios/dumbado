ecea419e30c87b430db9a84a8fae83feeffc9e58
Ricard Pradell <mombars@users.berlios.de>
2010-11-23 04:34:50 +0100

    Author:    Ricard Pradell <mombars@users.berlios.de>  On branch master  Complete english translation

--- dumbado_nogui/src/main.c
+++ dumbado_nogui/src/main.c
@@ -29,98 +29,98 @@
 #include <netinet/ether.h> 
 #include <netinet/ip.h> 
 
-/* la cabecera tcpdump (ether.h) define ETHER_HDRLEN) */
+/* tcpdump header (ether.h) defines ETHER_HDRLEN) */
 #ifndef ETHER_HDRLEN 
 #define ETHER_HDRLEN 14
 #endif
 
-/* prototipos de las funciones "handle" para cabeceras ethernet y IP */
+/* handle function prototypes for IP and ethernet headers */
 u_int16_t handle_ethernet
         (u_char *args,const struct pcap_pkthdr* pkthdr,const u_char*
-        paquete);
+        packet);
 u_char* handle_IP
         (u_char *args,const struct pcap_pkthdr* pkthdr,const u_char*
-        paquete);
+        packet);
 
 
-/* Estructura de una cabecera IP */
-struct ip_mio {
-	u_int8_t	ip_vhl;		/* longitud cabecera, versión */
+/* IP header struct */
+struct my_ip {
+	u_int8_t	ip_vhl;		/* header length, version */
 #define IP_V(ip)	(((ip)->ip_vhl & 0xf0) >> 4)
 #define IP_HL(ip)	((ip)->ip_vhl & 0x0f)
-	u_int8_t	ip_tos;		/* tipo de servicio */
-	u_int16_t	ip_len;		/* longitud total */
-	u_int16_t	ip_id;		/* identificación */
-	u_int16_t	ip_off;		/* campo de desplazamiento del fragmento */
-#define	IP_DF 0x4000			/* flag "no fragmentar" */
-#define	IP_MF 0x2000			/* flag "más fragmentos" */
-#define	IP_OFFMASK 0x1fff		/* máscara para fragmentar bits */
-	u_int8_t	ip_ttl;		/* tiempo de vida */
-	u_int8_t	ip_p;		/* protocolo */
+	u_int8_t	ip_tos;		/* type of service */
+	u_int16_t	ip_len;		/* total length */
+	u_int16_t	ip_id;		/* identification */
+	u_int16_t	ip_off;		/* fragment 's offset field */
+#define	IP_DF 0x4000			/* "don't fragment" flag */
+#define	IP_MF 0x2000			/* "more fragments" flag */
+#define	IP_OFFMASK 0x1fff		/* mask for bit fragmentation */
+	u_int8_t	ip_ttl;		/* time to live */
+	u_int8_t	ip_p;		/* protocol */
 	u_int16_t	ip_sum;		/* checksum */
 	struct in_addr ip_orig;
-	struct in_addr ip_dst;	/* direcciones origen y destino */
+	struct in_addr ip_dst;	/* origin and destiny adresses */
 };
 
-/* función de callback que analizará la cabecera ethernet */
-void callback_mio(u_char *args,const struct pcap_pkthdr* pkthdr,const u_char*
-        paquete)
+/* callback function that will analyze ethernet header */
+void my_callback(u_char *args,const struct pcap_pkthdr* pkthdr,const u_char*
+        packet)
 {
-   u_int16_t tipo = handle_ethernet(args,pkthdr,paquete);
+   u_int16_t type = handle_ethernet(args,pkthdr,packet);
 	
-/* handle de paquetes IP. Otros handlers pueden añadirse aquí */
-    if(tipo == ETHERTYPE_IP)
+/* IP packet handler. Other handles can be added here */
+    if(type == ETHERTYPE_IP)
     {
-        handle_IP(args,pkthdr,paquete);
+        handle_IP(args,pkthdr,packet);
     }
 }
 
 u_char* handle_IP
         (u_char *args,const struct pcap_pkthdr* pkthdr,const u_char*
-        paquete)
+        packet)
 {
-    const struct ip_mio* ip;
-    u_int longitud = pkthdr->len;
+    const struct my_ip* ip;
+    u_int length = pkthdr->len;
     u_int caplen,off,version;
     int len;
 
-    /* salta la cabecera ethernet */
-    ip = (struct ip_mio*)(paquete + sizeof(struct ether_header));
-    longitud -= sizeof(struct ether_header);
+    /* skips ethernet header */
+    ip = (struct my_ip*)(packet + sizeof(struct ether_header));
+    length -= sizeof(struct ether_header);
 
-   /* comprueba si el paquete tiene la longitud correcta */
-    if (longitud < sizeof(struct ip_mio))
+   /* checks if packet has the correct size */
+    if (length < sizeof(struct my_ip))
     {
-        printf("ip truncada %d",longitud);
+        printf("truncated ip %d",length);
         return NULL;
     }
 
 	len = ntohs(ip->ip_len);
-    caplen = IP_HL(ip); /* longitud de la cabecera */
-    version = IP_V(ip);/* versión ip */
+    caplen = IP_HL(ip); /* header length */
+    version = IP_V(ip);/* ip version*/
 	
-    /* comprueba versión */
+    /* checks version */
     if(version != 4)
     {
-      fprintf(stdout,"Versión desconocida %d\n",version);
+      fprintf(stdout,"Unknown version %d\n",version);
       return NULL;
     }
 
-    /* comprueba longitud cabecera */
+    /* checks header length */
     if(caplen < 5 )
     {
-        fprintf(stdout,"longitud cabecera errónea %d \n",caplen);
+        fprintf(stdout,"wrong header length %d \n",caplen);
     }
 
-    /* comprueba la longitud del paquete */
-    if(longitud < len)
-        printf("\nIP truncada - faltan %d bytes\n",len - longitud);
+    /* checks packet length */
+    if(length < len)
+        printf("\ntruncated IP - %d bytes missing\n",len - length);
 
-    /* comprueba si tenemos el primer fragmento */
+    /* checks if we have the first fragment */
     off = ntohs(ip->ip_off);
-    if((off & 0x1fff) == 0 )/* no hay 1's en los primeros 13 bits */
+    if((off & 0x1fff) == 0 )/* no 1's in the first 13 bits */
     {
-		/* muestra ORIGEN DESTINO caplen version len */        
+		/* shows ORIG DEST caplen version len */        
 		fprintf(stdout,"IP: ");
         fprintf(stdout,"%s ",
                 inet_ntoa(ip->ip_orig));
@@ -132,44 +132,44 @@ u_char* handle_IP
     return NULL;
 }
 
-/* procesa paquetes ethernet */
+/* process ethernet packets */
 u_int16_t handle_ethernet
         (u_char *args,const struct pcap_pkthdr* pkthdr,const u_char*
-        paquete)
+        packet)
 {
     u_int eth_caplen = pkthdr->caplen;
-    u_int eth_longitud = pkthdr->len;
+    u_int eth_length = pkthdr->len;
     struct ether_header *eptr;  /* net/ethernet.h */
-    u_short ether_tipo;
+    u_short ether_type;
 
     if (eth_caplen < ETHER_HDRLEN)
     {
-        fprintf(stdout,"Longitud del paquete menor que la\nlongitud de la cabecera ethernet\n");
+        fprintf(stdout,"Packet length shorter than ethernet header\n");
         return -1;
     }
 
-    /* procesamos la cabecera ethernet... */
-    eptr = (struct ether_header *) paquete;
-    ether_tipo = ntohs(eptr->ether_type);
+    /* process ethernet header... */
+    eptr = (struct ether_header *) packet;
+    ether_type = ntohs(eptr->ether_type);
 
-    /* mostramos ORIGEN DESTINO TIPO LONGITUD */
+    /* show ORIG DEST TYPE LENGTH */
     fprintf(stdout,"ETH: ");
     fprintf(stdout,"%s "
             ,ether_ntoa((struct ether_addr*)eptr->ether_shost));
     fprintf(stdout,"%s "
             ,ether_ntoa((struct ether_addr*)eptr->ether_dhost));
 
-    /* comprueba si el paquete es IP */
-    if (ether_tipo == ETHERTYPE_IP)
+    /* checks if packet is IP */
+    if (ether_type == ETHERTYPE_IP)
     {
         fprintf(stdout,"(IP)");
     }else {
-    /*desconocido (todo lo que no sea lo anterior...) */
+    /*unknown (everything else...) */
         fprintf(stdout,"(?)");
     }
-    fprintf(stdout," %d\n",eth_longitud);
+    fprintf(stdout," %d\n",eth_length);
 
-    return ether_tipo;
+    return ether_type;
 }
 
 
@@ -178,30 +178,30 @@ int main(int argc,char **argv)
     char *dev; 
     char errbuf[PCAP_ERRBUF_SIZE];
     pcap_t* descr;
-    struct bpf_program fp;      /* estructura del programa BPF compilado */
-    bpf_u_int32 masc;          /* mascara */
-    bpf_u_int32 la_red;           /* ip */
+    struct bpf_program fp;      /* compiled BPF program struct */
+    bpf_u_int32 mask;          /* mask */
+    bpf_u_int32 net;           /* ip */
     u_char* args = NULL;
 
-    /* de momento pasamos las opciones de filtrado como una cadena */
+    /* we pass filtering options as a string by now */
     if(argc < 3){ 
-        fprintf(stdout,"Uso: %s <interfaz> <numero de paquetes> <\"cadena filtro\">\n",argv[0]);
+        fprintf(stdout,"Usage: %s <interface> <packets> <\"filter string\">\n",argv[0]);
         return 0;
     }
 
-    /* busca interfaz util... */
+    /* search useful interfaces... */
     /*dev = pcap_lookupdev(errbuf); 
-     * DE MOMENTO SE ENTRA LA INTERFAZ COMO OPCIÓN(eth0, eth1...) */
+     * WE ENTER INTERFACES AS AN OPTION BY NOW (eth0, eth1...) */
     dev = argv[1];
 
-    /*NO SE NECESITA DE MOMENTO */
+    /*NOT NEEDED BY NOW */
     /*if(dev == NULL)
     { printf("%s\n",errbuf); exit(1); }*/
 
-    /* pregunta a pcap por la dirección de red y la máscara de la tarjeta */
-    pcap_lookupnet(dev,&la_red,&masc,errbuf);
+    /* asks pcap device's net adress and mask */
+    pcap_lookupnet(dev,&net,&mask,errbuf);
 
-    /* abre la interfaz en modo lectura, por defecto, en modo promiscuo */
+    /* opens interface in read mode. By default, promiscuous mode */
     descr = pcap_open_live(dev,BUFSIZ,1,-1,errbuf);
     if(descr == NULL)
     { printf("pcap_open_live(): %s\n",errbuf); exit(1); }
@@ -209,19 +209,19 @@ int main(int argc,char **argv)
 
     if(argc > 3)
     {
-        /* Tratamos de compilar el filtro */
-        if(pcap_compile(descr,&fp,argv[3],0,la_red) == -1)
-        { fprintf(stderr,"Error en la llamada a pcap_compile\n"); exit(1); }
+        /* Try to compile filter */
+        if(pcap_compile(descr,&fp,argv[3],0,net) == -1)
+        { fprintf(stderr,"Error when calling pcap_compile\n"); exit(1); }
 
-        /* inicializa el programa compilado como filtro */
+        /* initiates compiled program as a filter */
         if(pcap_setfilter(descr,&fp) == -1)
-        { fprintf(stderr,"Error inicializando el filtro\n"); exit(1); }
+        { fprintf(stderr,"Error initializing filter\n"); exit(1); }
     }
 
-    /* bucle */ 
-    pcap_loop(descr,atoi(argv[2]),callback_mio,args);
+    /* loop */ 
+    pcap_loop(descr,atoi(argv[2]),my_callback,args);
 
-    fprintf(stdout,"\nfinalizado\n");
+    fprintf(stdout,"\nfinished\n");
     return 0;
 }
 
